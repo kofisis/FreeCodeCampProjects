b908d60869e9914ea583aa314159749c48b1f92e
kofisis <52378242+kofisis@users.noreply.github.com>
2020-05-06 11:30:04 +0200

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function palindrome(str) {
 
     //replace non-alphanumerics with space and convert to lowercase
@@ -12,18 +11,4 @@ function palindrome(str) {
         .join("")
     );
   }
-=======
-function palindrome(str) {
 
-    //replace non-alphanumerics with space and convert to lowercase
-    return (
-      str.replace(/[\W_]/g, "").toLowerCase() ===
-      str // the reversed of the above
-        .replace(/[\W_]/g, "")
-        .toLowerCase()
-        .split("")
-        .reverse()
-        .join("")
-    );
-  }
->>>>>>> 391afcf2f95094717fe37b8a8a4496341ef960f6
